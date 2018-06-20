5634b334366de4b72b8ede03198020ca619f2b57
Ronald Appleton <ronald.appleton@visualsoft.co.uk>
2018-06-20 19:31:35 +0100

    Asyn Client
    Builds async client and add a statobject for both server and client

---
+++ Starpeace/ClientServerProtocol/StateObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using System.Net.Sockets;
+
+namespace Starpeace.ClientServerProtocol
+{
+    public class StateObject
+    {
+        // Client socket.
+        public Socket workSocket = null;
+        // Size of receive buffer.
+        public int BufferSize { get; }
+        // Receive buffer.
+        public byte[] buffer;
+        // Received data string.
+        public StringBuilder sb = new StringBuilder();
+
+        public StateObject(int buffer_size=256)
+        {
+            BufferSize = buffer_size;
+            buffer = new byte[buffer_size];
+        }
+    }
+}
